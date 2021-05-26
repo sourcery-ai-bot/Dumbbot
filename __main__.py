5a422d983d21d9674b65f3d8b637ea26904a32dc
Sengolda <79252176+Sengolda@users.noreply.github.com>
2021-05-26 22:21:59 +0530

    added os env vars for Jishaku config.

--- __main__.py
+++ __main__.py
@@ -4,6 +4,9 @@ from discord import Color, Embed
 
 from bot import *
 
+os.environ.setdefault("JISHAKU_HIDE", "1")
+os.environ.setdefault("JISHAKU_RETAIN", "1")
+os.environ.setdefault("JISHAKU_NO_UNDERSCORE", "1")
 
 class Help_Command(commands.MinimalHelpCommand):
     async def send_pages(self):
